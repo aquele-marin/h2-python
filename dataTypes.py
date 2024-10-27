c5bb1f394aaadee54c6bc528245c3cf5252180cd
Gabriel Marin <gabrielvbmarin21@gmail.com>
2024-10-27 18:56:56 -0300

    add python data types

---
+++ dataTypes.py
@@ -0,0 +1,47 @@
+# Python Data Types
+
+# Built-in Data Types
+# Variables can store data of different types, and different types can do different things.
+# Python has the followings data types built-in by default, in these categories:
+
+# Text Type:	str
+text = "text"
+
+# Numeric Types:	int, float, complex
+number = 1
+float_number = 1.0
+complex_number = 1j
+
+# Sequence Types:	list, tuple, range
+list = ["apple", "banana", "cherry"]
+tuple = ("apple", "banana", "cherry")
+range = range(6)
+
+# Mapping Type:	dict
+dictionary = {"name": "John", "age": 36}
+
+# Set Types:	set, frozenset
+set = {"apple", "banana", "cherry"}
+frozenset = frozenset({"apple", "banana", "cherry"})
+
+# Sets are unordered collections of unique elements, used to remove duplicates from a list and math operations. Key characteristics:
+
+# No duplicate elements
+# Mutable (can be modified)
+# Elements must be immutable
+# Optimized for membership testing
+
+# Boolean Type:	bool
+boolean = True
+
+# Binary Types:	bytes, bytearray, memoryview
+bytes = b"Hello"
+bytearray = bytearray(5)
+memoryview = memoryview(bytes)
+
+# We can use Binary Type to solve file management, network programming and image processing problems
+
+# None Type:	NoneType
+none = None
+
+# Whats the difference of 
